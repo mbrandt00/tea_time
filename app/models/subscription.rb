bcb2286e225f988f2325eba6db8fe87ffeccd31f
Michael Brandt <msbrandt00@gmail.com>
2022-06-06 13:38:59 -0600

    Feature: model associations and validations

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,7 @@
 class Subscription < ApplicationRecord
-  belongs_to :customer
-  belongs_to :tea
+    belongs_to :customer
+    belongs_to :tea
+    validates_presence_of :title, :price, :status, :frequency
+    enum status: [:active, :cancelled]
+    enum frequency: [:weekly, :monthly, :quarterly, :yearly]
 end
--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,2 +1,5 @@
 class Tea < ApplicationRecord
+    has_many :subscriptions, dependent: :destroy
+    has_many :customers, through: :subscriptions
+    validates_presence_of :title
 end
